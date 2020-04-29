5c5df911521de3a960e5d738825e0160ea84e624
nateptay <natetay@outlook.com>
2020-04-29 11:02:39 -0700

    adds animated loading options

--- animate-svg.js
+++ animate-svg.js
@@ -1,3 +1,19 @@
 /* ****************************************** */
 /* Progress Circle Animation using SVG  */ 
 /*      Add CodePen JavaScript code here     */
+var count = $(('#count'));
+$({ Counter: 0 }).animate({ Counter: count.text() }, {
+  duration: 2000,
+  easing: 'linear',
+  step: function () {
+    count.text(Math.ceil(this.Counter)+ "%");
+  }
+});
+
+var s = Snap('#animated');
+var progress = s.select('#progress');
+
+progress.attr({strokeDasharray: '0, 251.2'});
+Snap.animate(0,251.2, function( value ) {
+    progress.attr({ 'stroke-dasharray':value+',251.2'});
+}, 2000);
